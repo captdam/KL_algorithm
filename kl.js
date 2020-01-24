51436fe4ffe2368672d61bdbdc4292295ccfe4f5
Captdam <captdam@beardle.com>
2020-01-24 05:31:25 -0500

    Util: Find D value
    + Hardcode the test files for faster debugging

--- kl.js
+++ kl.js
@@ -1,14 +1,51 @@
 'use srict';
 /* KL utils*/
 
-//Get the number of inter-connect edges in this design (cut-cost)
-/*function countInterConnect(design,netlist) {
-	var interConnect = 0;
+//Get the D, I and E value for all nodes in the design
+function getDIE(design,netlist) {
+	['groupA','groupB'].map( group => {
+		for (var i = 0; i < design[group].length; i++) { //Find D I E for all nodes
+			design[group][i].I = 0;
+			design[group][i].E = 0;
+			netlist.map( edge => { //For each node in the design, check all edges
+//				console.log('I'+design[group][i].I,'E'+design[group][i].E);
+//				console.log('Check node '+design[group][i].name+' edge ',edge.nodes);
+				//Case 1 - This edge is not connected to this node
+				if (!edge.nodes.includes(design[group][i].name))
+					return;
+				//Then, consider all nodes connected to this edge, except this node
+				var edgeWithoutThisNode = {
+					"nodes" : edge.nodes.filter( x => {return x != design[group][i].name} )
+				};
+				//Case 2 - This edge connected nodes both inside and outside of the group
+				if (isInterConnect(design,edgeWithoutThisNode)) {
+					design[group][i].I += edge.weight;
+					design[group][i].E += edge.weight;
+					return;
+				}
+				//Case 3 - This edge connected nodes only in / out of the current group
+				if ( getNodeGroup(design,design[group][i].name) == getNodeGroup(design,edgeWithoutThisNode.nodes[0]) ) //Same group
+					design[group][i].I += edge.weight;
+				else
+					design[group][i].E += edge.weight;
+				
+			} );
+			design[group][i].D = design[group][i].E - design[group][i].I;
+		}
+	} );
+}
+
+//Get the weighted cost of inter-connect edges in this design (cut-cost)
+function countInterConnect(design,netlist) {
+	var interConnect = 0, cost = 0;
 	netlist.map( x => { //Check all edges
-		if ( isInterConnect(design,x) ) interConnect++;
+		if ( isInterConnect(design,x) ) {
+			interConnect++;
+			cost += x.weight;
+		}
 	} );
 	return interConnect;
-}*/
+}
 
 //Test if a edge connects nodes from more than one group
 function isInterConnect(design,edge) {
